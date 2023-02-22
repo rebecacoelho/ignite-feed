3b0cb35157d01aae5a4941d19f7995090b8c3bdb
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2023-02-22 12:43:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar/Sidebar"
+import { Post } from "./components/Post/Post"
+
+import './global.css'
+import styles from  './App.module.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/rebecacoelho.png",
+      name: "Rebeca Coelho",
+      role: "Desenvolvedora Front-End"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-02-14 15:37:20')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "Educator @rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-02-10 19:00:00')
+  }
+]
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          {posts.map(post => {
+            return (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />)
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
